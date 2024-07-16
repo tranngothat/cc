b5fece704a7d2fe244048403fffe6af16a80c58f
tranngothat <ngothat0gmail.com>
2024-07-16 23:22:27 +0700

    jjjj

---
+++ src/testthi/sachh.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testthi;
+
+/**
+ *
+ * @author DELL
+ */
+public class sachh {
+    String masach;
+    String tensach;
+    String tacgia;
+    String theloai;
+    String nhaXB;     
+
+    public sachh() {
+    }
+
+    public sachh(String masach, String tensach, String tacgia, String theloai, String nhaXB) {
+        this.masach = masach;
+        this.tensach = tensach;
+        this.tacgia = tacgia;
+        this.theloai = theloai;
+        this.nhaXB = nhaXB;
+    }
+
+    public String getMasach() {
+        return masach;
+    }
+
+    public void setMasach(String masach) {
+        this.masach = masach;
+    }
+
+    public String getTensach() {
+        return tensach;
+    }
+
+    public void setTensach(String tensach) {
+        this.tensach = tensach;
+    }
+
+    public String getTacgia() {
+        return tacgia;
+    }
+
+    public void setTacgia(String tacgia) {
+        this.tacgia = tacgia;
+    }
+
+    public String getTheloai() {
+        return theloai;
+    }
+
+    public void setTheloai(String theloai) {
+        this.theloai = theloai;
+    }
+
+    public String getNhaXB() {
+        return nhaXB;
+    }
+
+    public void setNhaXB(String nhaXB) {
+        this.nhaXB = nhaXB;
+    }
+}
+
